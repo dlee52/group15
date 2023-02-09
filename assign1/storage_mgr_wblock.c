a8b2b4f4cbdad4891fb08751c3675f21eaee36e2
vmishra07 <123329482+vmishra07@users.noreply.github.com>
2023-02-09 17:23:44 -0600

    Update storage_mgr_wblock.c

--- assign1/storage_mgr_wblock.c
+++ assign1/storage_mgr_wblock.c
@@ -7,20 +7,21 @@
  *                   by Vatsal
  * +----------------+----------------------------------------------------------+
  */
+
 #include "storage_mgr.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 // Writing the page to a Disk by using absolute count
 RC writeBlockToDisk(int pageNum, SM_FileHandle *fileHandle, SM_PageHandle memoryPage) {
-/*
   if (!fileHandle) return RC_FILE_HANDLE_NOT_INIT;
   FILE *fp = getFileDescriptor(fileHandle);
   if (!fp) return RC_FILE_NOT_FOUND;
 
   ensureSufficientCapacity(pageNum, fileHandle);
 
-  int pageLocation = pageNum * PAGE_SIZE;
+  int offset = sizeof(MGMT_Info);
+  int pageLocation = offset + (pageNum * PAGE_SIZE);
   if (fseek(fp, pageLocation, SEEK_SET) != 0) return RC_WRITE_FAILED;
   // creating a block
 
@@ -30,32 +31,28 @@ RC writeBlockToDisk(int pageNum, SM_FileHandle *fileHandle, SM_PageHandle memory
   // Now we have to update the overall number of pages.
 
   int fileSize = ftell(fp);
-  int totalPages = fileSize / PAGE_SIZE;
-  if (fileSize % PAGE_SIZE != 0) ++totalPages;
+  int totalPages = (fileSize - offset) / PAGE_SIZE;
+  if ((fileSize - offset) % PAGE_SIZE != 0) ++totalPages;
   fileHandle->totalNumPages = totalPages;
 
-*/
   return RC_OK;
 }
 // Now we have to write the current block
 RC writeCurrentBlockToFile(SM_FileHandle *fileHandle, SM_PageHandle memoryPage) {
-/*
   if (!fileHandle) return RC_FILE_HANDLE_NOT_INIT;
   int currPage = fileHandle->curPagePos;
   return writeBlockToDisk(currPage, fileHandle, memoryPage);
-*/
-return RC_OK;
 }
 //Here we are appending the empty block
 RC appendEmptyPage(SM_FileHandle *fileHandle) {
-/*
   if (!fileHandle) return RC_FILE_HANDLE_NOT_INIT;
   int newPage = fileHandle->totalNumPages;
 
   FILE *fp = getFileDescriptor(fileHandle);
   if (!fp) return RC_FILE_NOT_FOUND;
 
-  int pageLocation = newPage * PAGE_SIZE;
+  int offset = sizeof(MGMT_Info);
+  int pageLocation = offset + (newPage * PAGE_SIZE);
   if (fseek(fp, pageLocation, SEEK_SET) != 0) return RC_WRITE_FAILED;
 
   char *emptyMemory = calloc(PAGE_SIZE, sizeof(char));
@@ -64,19 +61,12 @@ RC appendEmptyPage(SM_FileHandle *fileHandle) {
   free(emptyMemory);
 
   ++fileHandle->totalNumPages;
-*/
 
   return RC_OK;
 }
 // Here we have to ensure that there is sufficient capacity
 RC ensureSufficientCapacity(int numPages, SM_FileHandle *fileHandle) {
-/*
   if (!fileHandle) return RC_FILE_HANDLE_NOT_INIT;
   while (numPages > fileHandle->totalNumPages) {
-    if (appendEmptyPage(fileHandle) != RC_OK) return RC_WRITE_FAILED;
-  }
-*/
-  return RC_OK;
-}
-
-
+    if (appendEmptyPage(fileHandle) != RC_OK) 
+return RC_WRITE_FAILED
